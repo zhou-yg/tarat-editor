e8189b3c08e865aacca528465ed0842c27b5f770
zhouyunge <zhouyunge@kuaishou.com>
2022-09-29 16:30:44 +0800

    feat: compile module tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./views/**/*.{js,ts,jsx,tsx}",
+    "node_modules/tarat-*/dist/views/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
